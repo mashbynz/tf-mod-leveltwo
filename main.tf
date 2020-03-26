0a9fe2306c0e40dd92ef40a81e01dc7880b6f1b8
Matt Ashby <matt@ashbys.nz>
2020-03-26 22:27:20 +1300

    added NIC and VM

--- main.tf
+++ main.tf
@@ -1,98 +1,57 @@
-resource "azurerm_virtual_machine" "vm" {
-  name                  = module.caf_name_vm.gen
-  resource_group_name   = var.resource_group_name
-  location              = var.location
-  vm_size               = var.vm_size
-  tags                  = local.tags
-  network_interface_ids = var.network_interface_ids
+resource "azurerm_network_interface" "nic" {
+  for_each = var.vm_object.nics
 
-  delete_os_disk_on_termination = true
+  name                = "${each.value.name}${var.nic_suffix}"
+  resource_group_name = each.value.resource_group_name
+  location            = each.value.location
 
-  primary_network_interface_id = var.primary_network_interface_id
-
-  os_profile {
-    computer_name  = module.caf_name_vm.gen
-    admin_username = var.os_profile.admin_username
-    admin_password = lookup(var.os_profile, "admin_password", null)
-  }
-
-  // Reference a marketplace image
-  dynamic "storage_image_reference" {
-    for_each = lookup(var.storage_image_reference, "id", null) == null ? [1] : []
+  dynamic "ip_configuration" {
+    for_each = lookup(each.value, "ip_configuration", {}) #!= {} ? [1] : []
 
     content {
-      publisher = var.storage_image_reference.publisher
-      offer     = var.storage_image_reference.offer
-      sku       = var.storage_image_reference.sku
-      version   = var.storage_image_reference.version
+      name                          = lookup(ip_configuration.value, "name", null)
+      subnet_id                     = var.level0_subnet[lookup(ip_configuration.value, "subnet_id", null)].id
+      private_ip_address_allocation = lookup(ip_configuration.value, "private_ip_address_allocation", null)
     }
   }
 
-  // Reference an image gallery ID
-  dynamic "storage_image_reference" {
-    for_each = lookup(var.storage_image_reference, "id", null) == null ? [] : [1]
+  depends_on = [
+    var.level0_vnet
+  ]
+}
 
-    content {
-      id = var.storage_image_reference.id
-    }
+resource "azurerm_windows_virtual_machine" "vm" {
+  for_each = var.vm_object.vms
+
+  name                  = "${each.value.name}${var.vm_suffix}"
+  resource_group_name   = each.value.resource_group_name
+  location              = each.value.location
+  size                  = each.value.size
+  admin_username        = each.value.admin_username
+  admin_password        = each.value.admin_password
+  network_interface_ids = [azurerm_network_interface.nic[each.value.network_interface_ids].id]
+  license_type          = each.value.os_profile.license_type
+  provision_vm_agent    = each.value.os_profile.provision_vm_agent
+
+  os_disk {
+    name                 = "${each.value.name}${var.os_disk_suffix}"
+    caching              = each.value.storage_os_disk.caching
+    storage_account_type = each.value.storage_os_disk.storage_account_type
+    disk_size_gb         = each.value.storage_os_disk.disk_size_gb
   }
 
-  dynamic "storage_os_disk" {
-    for_each = var.storage_os_disk == null ? [] : [1]
-
-    content {
-      name                      = var.storage_os_disk.name
-      managed_disk_type         = var.storage_os_disk.managed_disk_type
-      caching                   = var.storage_os_disk.caching
-      create_option             = var.storage_os_disk.create_option
-      disk_size_gb              = var.storage_os_disk.disk_size_gb
-      write_accelerator_enabled = lookup(var.storage_os_disk, "write_accelerator_enabled", null)
-    }
+  source_image_reference {
+    publisher = each.value.storage_image_reference.publisher
+    offer     = each.value.storage_image_reference.offer
+    sku       = each.value.storage_image_reference.sku
+    version   = each.value.storage_image_reference.version
   }
 
-  dynamic "os_profile_linux_config" {
-    for_each = lower(var.os) == "linux" ? [1] : []
-
-    content {
-      disable_password_authentication = true
-
-      // TODO: ssh key management to be in external module
-      ssh_keys {
-        path     = "/home/${var.os_profile.admin_username}/.ssh/authorized_keys"
-        key_data = tls_private_key.ssh.public_key_openssh
-      }
-    }
+  boot_diagnostics {
+    storage_account_uri = var.governance_storage_accounts[each.value.boot_diagnostics.storage_account_uri].primary_blob_endpoint
   }
 
-  dynamic "os_profile_windows_config" {
-    for_each = lower(var.os) == "windows" ? [1] : []
-
-    content {
-      provision_vm_agent        = lookup(var.os_profile, "provision_vm_agent", null)
-      enable_automatic_upgrades = lookup(var.os_profile, "enable_automatic_upgrades", null)
-      timezone                  = lookup(var.os_profile, "timezone", null)
-    }
-  }
-
-  dynamic "os_profile_secrets" {
-    for_each = var.os_profile_secrets == null ? [] : [1]
-
-    content {
-      source_vault_id = var.os_profile_secrets.source_vault_id
-      vault_certificates {
-        certificate_url   = var.os_profile_secrets.vault_certificates.certificate_url
-        certificate_store = lookup(var.os_profile_secrets.vault_certificates, "certificate_store", null)
-      }
-    }
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  license_type = lookup(var.os_profile, "license_type", null)
-
-  provisioner "local-exec" {
-    command = "az vm restart --name ${azurerm_virtual_machine.vm.name} --resource-group ${var.resource_group_name}"
-  }
+  depends_on = [
+    azurerm_network_interface.nic
+  ]
 }
